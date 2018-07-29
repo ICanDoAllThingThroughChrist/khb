b076fcb58010a9d9eeafc5676c8b0a744ab3473b
github username <github email address>
2018-07-29 01:10:32 -0500

    following-rails-angular-postgres-bootstrap-2nd-edition

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180728142511) do
+ActiveRecord::Schema.define(version: 20180729044749) do
 
   create_table "councils", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "username", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["username"], name: "index_customers_on_username", unique: true
   end
 
   create_table "organizations", force: :cascade do |t|
@@ -49,6 +60,23 @@ ActiveRecord::Schema.define(version: 20180728142511) do
     t.datetime "updated_at", null: false
     t.index ["council_id"], name: "index_requests_on_council_id"
     t.index ["organization_id"], name: "index_requests_on_organization_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
